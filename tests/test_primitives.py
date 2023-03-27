f8b17a576df9adabcb0b26ac6f9e75deb1058d14
Yannick Augenstein <yannick.augenstein@me.com>
2023-03-27 12:56:51 +0200

    Add forward and backward sparse solve autograd primitives

---
+++ tests/test_primitives.py
@@ -0,0 +1,76 @@
+import numpy as np
+import pytest
+from autograd.test_util import check_grads
+from numpy.testing import assert_allclose
+from scipy.sparse import coo_matrix, csc_matrix
+from scipy.sparse.linalg import splu, spsolve
+
+from tofea.primitives import solve_coo, solve_lu
+
+
+@pytest.fixture
+def rng():
+    seed = 36523523
+    return np.random.default_rng(seed)
+
+
+@pytest.mark.parametrize("n", [10, 11])
+def test_solve_lu(rng, n):
+    m = rng.random((n, n))
+    mx = np.sum(np.abs(m), axis=1)
+    np.fill_diagonal(m, mx)
+    m = csc_matrix(m)
+
+    b = rng.random(n)
+
+    f = splu(m)
+    x0 = f.solve(b)
+
+    x1 = solve_lu(f.L, f.U, b)
+
+    assert_allclose(x0, x1)
+
+
+@pytest.mark.parametrize("n", [10, 11])
+def test_solve_coo(rng, n):
+    m = rng.random((n, n))
+    mx = np.sum(np.abs(m), axis=1)
+    np.fill_diagonal(m, mx)
+    m = coo_matrix(m)
+
+    b = rng.random(n)
+
+    x0 = spsolve(m.tocsr(), b)
+    x1 = solve_coo(m.data, (m.row, m.col), b)
+
+    assert_allclose(x0, x1)
+
+
+@pytest.mark.parametrize("n", [10, 11])
+@pytest.mark.parametrize("mode", ["fwd", "rev"])
+def test_solve_coo_entries_grad(rng, n, mode):
+    m = rng.random((n, n))
+    mx = np.sum(np.abs(m), axis=1)
+    np.fill_diagonal(m, mx)
+    m = coo_matrix(m)
+
+    b = rng.random(n)
+
+    check_grads(lambda x: solve_coo(x, (m.row, m.col), b), modes=[mode], order=1)(
+        m.data
+    )
+
+
+@pytest.mark.parametrize("n", [10, 11])
+@pytest.mark.parametrize("mode", ["fwd", "rev"])
+def test_solve_coo_b_grad(rng, n, mode):
+    m = rng.random((n, n))
+    mx = np.sum(np.abs(m), axis=1)
+    np.fill_diagonal(m, mx)
+    m = coo_matrix(m)
+
+    b = rng.random(n)
+
+    check_grads(lambda x: solve_coo(m.data, (m.row, m.col), x), modes=[mode], order=1)(
+        b
+    )
--- tofea/primitives.py
+++ tofea/primitives.py
@@ -1,28 +1,50 @@
-from autograd.extend import defvjp, primitive
-from scipy.sparse.linalg import coo_matrix, factorized
+from autograd.extend import defjvp, defvjp, primitive
+from scipy.sparse import coo_matrix
+from scipy.sparse.linalg import splu, spsolve_triangular
 
 _ctx = {}
+
+
+def solve_lu(L, U, b):
+    y = spsolve_triangular(L.tocsr(), b, lower=True)
+    x = spsolve_triangular(U.tocsr(), y, lower=False)
+    return x
 
 
 @primitive
 def solve_coo(entries, indices, rhs):
     a = coo_matrix((entries, indices)).tocsc()
-    _ctx["factorization"] = factorized(a)
-    return _ctx["factorization"](rhs)
+    _ctx["factorization"] = splu(a)
+    return _ctx["factorization"].solve(rhs)
+
+
+def solve_coo_entries_jvp(g, x, entries, indices, b):
+    a = coo_matrix((g, indices)).tocsc()
+    return _ctx["factorization"].solve(-(a @ x))
+
+
+def solve_coo_b_jvp(g, x, entries, indices, b):
+    return _ctx["factorization"].solve(g)
+
+
+defjvp(solve_coo, solve_coo_entries_jvp, None, solve_coo_b_jvp)
 
 
 def solve_coo_entries_vjp(ans, entries, indices, b):
     def vjp(g):
-        adjoint = _ctx["factorization"](g)  # TODO: generalize using U^T L^T!
+        f = _ctx["factorization"]
+        x = solve_lu(f.U.T, f.L.T, g)
         i, j = indices
-        return -adjoint[i] * ans[j]
+        return -x[i] * ans[j]
 
     return vjp
 
 
 def solve_coo_b_vjp(ans, entries, indices, b):
     def vjp(g):
-        return _ctx["factorization"](g)
+        f = _ctx["factorization"]
+        x = solve_lu(f.U.T, f.L.T, g)
+        return x
 
     return vjp
 
