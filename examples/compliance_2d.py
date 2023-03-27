3356a47d567da3b44c4a7cb7b3cad1e8cae6707b
Yannick Augenstein <yannick.augenstein@me.com>
2023-03-27 18:28:28 +0200

    Update examples and fix volume constraint, add self weight to compliance example

--- examples/compliance_2d.py
+++ examples/compliance_2d.py
@@ -3,18 +3,17 @@
 import autograd.numpy as np
 import matplotlib.pyplot as plt
 import nlopt
-from autograd import tensor_jacobian_product, value_and_grad
-from matplotlib.colors import CenteredNorm
+from autograd import value_and_grad
 
 from tofea.fea2d import FEA2D_K
 from tofea.topopt_helpers import simp_parametrization
 
 max_its = 100
 volfrac = 0.4
-sigma = 1.0
+sigma = 0.5
 shape = (160, 80)
 nelx, nely = shape
-emin, emax = 1e-4, 1
+emin, emax = 1e-6, 1
 
 dofs = np.arange(2 * (nelx + 1) * (nely + 1)).reshape(nelx + 1, nely + 1, 2)
 fixed = np.zeros_like(dofs, dtype=bool)
@@ -23,55 +22,53 @@ load = np.zeros_like(dofs)
 fixed[0, :, :] = 1
 load[-1, -1, 1] = 1
 
-fem = FEA2D_K(fixed, load)
+fem = FEA2D_K(fixed)
 parametrization = simp_parametrization(shape, sigma, emin, emax)
 x0 = np.full(shape, volfrac)
 
+
+def objective(x):
+    x = parametrization(x)
+    xp = np.pad(x, (0, 1), mode="constant", constant_values=emin)
+    xp = (xp - emin) / (emax - emin)
+    body = 1e-3 * np.stack([np.zeros_like(xp), xp], axis=-1)
+    x = fem(x, load + body)
+    return x
+
+
+def volume(x):
+    x = parametrization(x)
+    x = (x - emin) / (emax - emin)
+    return np.mean(x)
+
+
 plt.ion()
-fig, ax = plt.subplots(4, 1)
-im1 = ax[0].imshow(parametrization(x0).T, cmap="gray_r", vmin=emin, vmax=emax)
-ax[1].imshow(np.zeros_like(x0), cmap="magma")
-ax[2].imshow(np.zeros_like(load[..., 0]), cmap="RdBu", norm=CenteredNorm())
-ax[3].imshow(np.zeros_like(load[..., 1]), cmap="RdBu", norm=CenteredNorm())
+fig, ax = plt.subplots(1, 1)
+im = ax.imshow(parametrization(x0).T, cmap="gray_r", vmin=emin, vmax=emax)
 fig.tight_layout()
 
 
 def volume_constraint(x, gd):
-    v, g = value_and_grad(np.mean)(x)
+    v, g = value_and_grad(volume)(x)
     if gd.size > 0:
         gd[:] = g
     return v - volfrac
 
 
 def nlopt_obj(x, gd):
-    design = parametrization(x)
-    c, dc = value_and_grad(fem)(design)
+    c, dc = value_and_grad(objective)(x)
 
     if gd.size > 0:
-        gd[:] = tensor_jacobian_product(parametrization)(x, dc).ravel()
+        gd[:] = dc.ravel()
 
-    im1.set_data(design.T)
-    ax[1].cla()
-    ax[1].imshow(
-        design.T * np.reshape(fem._u, load.shape)[1:, 1:, 0].T,
-        cmap="RdBu",
-        norm=CenteredNorm(),
-    )
-    ax[2].cla()
-    ax[2].imshow(
-        design.T * np.reshape(fem._u, load.shape)[1:, 1:, 1].T,
-        cmap="RdBu",
-        norm=CenteredNorm(),
-    )
-    ax[3].cla()
-    ax[3].imshow(-fem._c.T, cmap="magma")
+    im.set_data(parametrization(x).T)
     plt.pause(0.01)
 
     return c
 
 
 opt = nlopt.opt(nlopt.LD_MMA, x0.size)
-opt.add_inequality_constraint(volume_constraint)
+opt.add_inequality_constraint(volume_constraint, 1e-6)
 opt.set_min_objective(nlopt_obj)
 opt.set_lower_bounds(0)
 opt.set_upper_bounds(1)
--- examples/heat_2d.py
+++ examples/heat_2d.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
-import autograd.numpy as anp
+import autograd.numpy as np
 import matplotlib.pyplot as plt
 import nlopt
-import numpy as np
-from autograd import tensor_jacobian_product, value_and_grad
+from autograd import value_and_grad
 
 from tofea.fea2d import FEA2D_T
 from tofea.topopt_helpers import simp_parametrization
 
 max_its = 100
-volfrac = 0.3
-sigma = 1.0
+volfrac = 0.4
+sigma = 0.5
 shape = (100, 100)
 nelx, nely = shape
 cmin, cmax = 1.0, 1e6
@@ -20,43 +19,52 @@ fixed = np.zeros((nelx + 1, nely + 1), dtype="?")
 load = np.zeros_like(fixed)
 
 fixed[40:60, -1] = 1
-load[:, 0] = 1
+load[:, :-10] = 1
 
-fem = FEA2D_T(fixed, load)
+fem = FEA2D_T(fixed)
 parametrization = simp_parametrization(shape, sigma, cmin, cmax)
 x0 = np.full(shape, volfrac)
 
+
+def objective(x):
+    x = parametrization(x)
+    x = fem(x, load)
+    return x
+
+
+def volume(x):
+    x = parametrization(x)
+    x = (x - cmin) / (cmax - cmin)
+    return np.mean(x)
+
+
 plt.ion()
-fig, ax = plt.subplots(2, 1)
-im1 = ax[0].imshow(parametrization(x0).T, cmap="gray_r", vmin=cmin, vmax=cmax)
-ax[1].imshow(np.zeros_like(x0), cmap="magma")
+fig, ax = plt.subplots(1, 1)
+im = ax.imshow(parametrization(x0).T, cmap="gray_r", vmin=cmin, vmax=cmax)
 fig.tight_layout()
 
 
 def volume_constraint(x, gd):
-    v, g = value_and_grad(anp.mean)(x)
+    v, g = value_and_grad(volume)(x)
     if gd.size > 0:
         gd[:] = g
     return v - volfrac
 
 
 def nlopt_obj(x, gd):
-    design = parametrization(x)
-    c, dc = value_and_grad(fem)(design)
+    c, dc = value_and_grad(objective)(x)
 
     if gd.size > 0:
-        gd[:] = tensor_jacobian_product(parametrization)(x, dc).ravel()
+        gd[:] = dc.ravel()
 
-    im1.set_data(design.T)
-    ax[1].cla()
-    ax[1].imshow((design * fem._c).T, cmap="magma")
+    im.set_data(parametrization(x).T)
     plt.pause(0.01)
 
     return c
 
 
 opt = nlopt.opt(nlopt.LD_MMA, x0.size)
-opt.add_inequality_constraint(volume_constraint)
+opt.add_inequality_constraint(volume_constraint, 1e-6)
 opt.set_min_objective(nlopt_obj)
 opt.set_lower_bounds(0)
 opt.set_upper_bounds(1)
