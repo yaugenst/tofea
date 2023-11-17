e73634bea9f032bb00450fcee19270a3a95abf36
Yannick Augenstein <yaugenst@google.com>
2023-11-17 10:23:27 -0800

    Add noqa's where appropriate

--- tofea/topopt_helpers.py
+++ tofea/topopt_helpers.py
@@ -5,7 +5,7 @@ from autograd.extend import defvjp, primitive
 gaussian_filter = primitive(scipy.ndimage.gaussian_filter)
 defvjp(
     gaussian_filter,
-    lambda ans, x, *args, **kwargs: lambda g: gaussian_filter(g, *args, **kwargs),
+    lambda ans, x, *args, **kwargs: lambda g: gaussian_filter(g, *args, **kwargs),  # noqa: ARG005
 )
 
 
