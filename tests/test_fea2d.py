7eb77cbd675f19c4aa5a5f4ca3ff11ab9f0c7955
Yannick Augenstein <yaugenst@google.com>
2023-11-20 09:47:49 -0800

    Fix tests and add tests for temperature, compliance, and displacement grads

--- tests/test_fea2d.py
+++ tests/test_fea2d.py
@@ -1,14 +1,28 @@
 import numpy as np
 import pytest
+from autograd.test_util import check_grads
 
 from tofea.fea2d import FEA2D_K, FEA2D_T
+
+
+@pytest.fixture()
+def rng():
+    seed = 36523523
+    return np.random.default_rng(seed)
 
 
 class TestFEA2DK:
     @pytest.fixture()
     def fea2d_k_instance(self):
         fixed = np.zeros((5, 5, 2), dtype=bool)
+        fixed[0] = 1
         return FEA2D_K(fixed)
+
+    @pytest.fixture()
+    def x_and_b(self, fea2d_k_instance, rng):
+        x = rng.random(fea2d_k_instance.shape)
+        b = rng.random(fea2d_k_instance.fixed.shape)
+        return x, b
 
     def test_shape(self, fea2d_k_instance):
         assert fea2d_k_instance.shape == (4, 4)
@@ -22,19 +36,49 @@ class TestFEA2DK:
     def test_fixdofs(self, fea2d_k_instance):
         fixdofs = fea2d_k_instance.fixdofs
         assert isinstance(fixdofs, np.ndarray)
-        assert fixdofs.size == 0
+        assert fixdofs.size == fea2d_k_instance.fixed[0].size
 
     def test_freedofs(self, fea2d_k_instance):
         freedofs = fea2d_k_instance.freedofs
         assert isinstance(freedofs, np.ndarray)
-        assert np.all(freedofs == np.arange(50))
+        assert freedofs.size == 50 - fea2d_k_instance.fixdofs.size
+
+    def test_displacement_grads(self, fea2d_k_instance, x_and_b):
+        x, b = x_and_b
+        check_grads(
+            lambda x_: fea2d_k_instance.displacement(x_, b),
+            modes=["fwd", "rev"],
+            order=1,
+        )(x)
+        check_grads(
+            lambda b_: fea2d_k_instance.displacement(x, b_),
+            modes=["fwd", "rev"],
+            order=1,
+        )(b)
+
+    def test_compliance_grads(self, fea2d_k_instance, x_and_b, rng):
+        x, _ = x_and_b
+        d = rng.random(fea2d_k_instance.dofs.shape)
+        check_grads(
+            lambda x_: fea2d_k_instance.compliance(x_, d), modes=["fwd", "rev"], order=1
+        )(x)
+        check_grads(
+            lambda d_: fea2d_k_instance.compliance(x, d_), modes=["fwd", "rev"], order=1
+        )(d)
 
 
 class TestFEA2DT:
     @pytest.fixture()
     def fea2d_t_instance(self):
         fixed = np.zeros((5, 5), dtype=bool)
+        fixed[0, 0] = 1
         return FEA2D_T(fixed)
+
+    @pytest.fixture()
+    def x_and_b(self, fea2d_t_instance, rng):
+        x = rng.random(fea2d_t_instance.shape)
+        b = rng.random(fea2d_t_instance.fixed.shape)
+        return x, b
 
     def test_shape(self, fea2d_t_instance):
         assert fea2d_t_instance.shape == (4, 4)
@@ -48,9 +92,22 @@ class TestFEA2DT:
     def test_fixdofs(self, fea2d_t_instance):
         fixdofs = fea2d_t_instance.fixdofs
         assert isinstance(fixdofs, np.ndarray)
-        assert fixdofs.size == 0
+        assert fixdofs.size == 1
 
     def test_freedofs(self, fea2d_t_instance):
         freedofs = fea2d_t_instance.freedofs
         assert isinstance(freedofs, np.ndarray)
-        assert np.all(freedofs == np.arange(25))
+        assert freedofs.size == 25 - fea2d_t_instance.fixdofs.size
+
+    def test_temperature_grads(self, fea2d_t_instance, x_and_b):
+        x, b = x_and_b
+        check_grads(
+            lambda x_: fea2d_t_instance.temperature(x_, b),
+            modes=["fwd", "rev"],
+            order=1,
+        )(x)
+        check_grads(
+            lambda b_: fea2d_t_instance.temperature(x, b_),
+            modes=["fwd", "rev"],
+            order=1,
+        )(b)
