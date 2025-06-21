30c315ccdcb2a48d5da8050678c7a8e4d7c12b12
Yannick Augenstein <1576526+yaugenst@users.noreply.github.com>
2025-06-21 11:01:43 +0200

    Fix API docs by listing modules

--- tofea/__init__.py
+++ tofea/__init__.py
@@ -1,5 +1,16 @@
 """Light-weight, differentiable finite element analysis package."""
 
+from __future__ import annotations
+
+__all__ = [
+    "elements",
+    "fea2d",
+    "primitives",
+    "solvers",
+    "DEFAULT_SOLVER",
+    "__version__",
+]
+
 __version__ = "0.1.0"
 
 DEFAULT_SOLVER = "SuperLU"
